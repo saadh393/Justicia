006d4362ee129c70fdcb498b5967bc5bc3195a04
Saad Hasan <saadh393@gmail.com>
2022-04-10 22:50:22 +0600

    our recent works

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#933DB5",
+        secondary: "#F8DFCB",
+      },
+    },
+  },
+  plugins: [],
+};
